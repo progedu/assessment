f6a81fc58472db67936ad114ea1011d53938df5d
yata78 <73291958+yata78@users.noreply.github.com>
2020-10-22 21:27:12 +0900

    Create assessment.css

---
+++ assessment.css
@@ -0,0 +1,17 @@
+body {
+  background-color: #04a6eb;
+  color: #fdffff;
+  width: 500px;
+  margin-right: auto;
+  margin-left: auto;
+}
+button {
+  padding: 5px 20px;
+  background-color: #337ab7;
+  color: #fdffff;
+  border-color: #2e6da4;
+  border-style: none;
+}
+input {
+  height: 20px;
+}
